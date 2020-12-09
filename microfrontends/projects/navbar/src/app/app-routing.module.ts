b2e901a6a30fccedf1cdb4956d6bd729004fe7b2
victor.montenegro <victor.montenegro@external.wolterskluwer.com>
2020-12-09 17:30:16 +0100

    + Deleted route tracing

--- microfrontends/projects/navbar/src/app/app-routing.module.ts
+++ microfrontends/projects/navbar/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { EmptyRouteComponent } from "./empty-route/empty-route.component";
 const routes: Routes = [{ path: "**", component: EmptyRouteComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
 })
